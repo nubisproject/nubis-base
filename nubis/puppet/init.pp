81a5418a5ec3ab05ddd6cb30cf993779e570495a
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-02-16 14:23:24 -0500

    Add a /etc/nubis-release file to images. Fixes issue #45

--- nubis/puppet/init.pp
+++ nubis/puppet/init.pp
@@ -6,3 +6,8 @@ import "fluentd.pp"
 import "jq.pp"
 import "packages.pp"
 import "postfix.pp"
+
+file { "/etc/nubis-release":
+  ensure => "present",
+  content => "nubis-base ${nubis_relase}.${nubis_build}\n"
+}
