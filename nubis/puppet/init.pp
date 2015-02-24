857f67ca524f45aa28887ea6ab76f0d7ec330c73
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-02-24 14:51:56 -0500

    puppet-lint

--- nubis/puppet/init.pp
+++ nubis/puppet/init.pp
@@ -1,14 +1,13 @@
-import "apt.pp"
-import "confd.pp"
-import "consul.pp"
-import "dnsmasq.pp"
-import "fluentd.pp"
-import "jq.pp"
-import "packages.pp"
-import "postfix.pp"
+import 'apt.pp'
+import 'confd.pp'
+import 'consul.pp'
+import 'dnsmasq.pp'
+import 'fluentd.pp'
+import 'jq.pp'
+import 'packages.pp'
+import 'postfix.pp'
 
-file {
-   '/etc/nubis-release':
-      ensure => 'present',
-      content => "nubis-base ${version}\n"
+file { '/etc/nubis-release':
+      ensure  => 'present',
+      content => "${project_name} ${project_version}\n"
 }
