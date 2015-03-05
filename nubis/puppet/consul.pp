0835002a71a527d1edf8ce4cb74ae207507d2a94
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-03-04 21:58:06 -0500

    Add envconsul, fixes issue #66

--- nubis/puppet/consul.pp
+++ nubis/puppet/consul.pp
@@ -10,3 +10,22 @@ class { 'consul':
       'enable_syslog' => true,
   }
 }
+
+# XXX: need to move to puppet-envconsul proper
+staging::file { 'envconsul.tar.gz':
+  source => "https://github.com/hashicorp/envconsul/releases/download/v0.5.0/envconsul_0.5.0_linux_amd64.tar.gz"
+} ->
+staging::extract { 'envconsul.tar.gz':
+  strip   => 0,
+  target  => "/opt/hashicorp",
+  creates => "/opt/hashicorp/envconsul_0.5.0_linux_amd64",
+} ->
+file { "/opt/hashicorp/envconsul_0.5.0_linux_amd64/envconsul":
+  owner =>  0,
+  group =>  0,
+  mode  => '0555',
+} ->
+file { "/usr/local/bin/envconsul"
+  ensure => "link",
+  target => '/opt/hashicorp/envconsul_0.5.0_linux_amd64/envconsul',
+}
