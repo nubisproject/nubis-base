5edd1c2a4cd98ac59c6225bd5d0ffc41f6ec1552
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-06-04 12:38:20 -0400

    Dont use Hiera

--- nubis/puppet/packages.pp
+++ nubis/puppet/packages.pp
@@ -1,5 +1,9 @@
-$allow_virtual_packages = hiera('allow_virtual_packages', false)
+# Silence this warning:
 
+# Warning: The package type's allow_virtual parameter will be changing its default value from
+# false to true in a future release. If you do not want to allow virtual packages, please
+# explicitly set allow_virtual to false.
+ 
 Package {
-  allow_virtual => $allow_virtual_packages,
+  allow_virtual => false,
 }
