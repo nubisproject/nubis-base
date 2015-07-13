195a41c714811b0ed69ca1a62122308053ae2767
limed <limed@mozilla.com>
2015-07-13 16:12:46 -0700

    Added some extra variables

--- nubis/bin/nubis-lib.sh
+++ nubis/bin/nubis-lib.sh
@@ -17,7 +17,9 @@ fi
 CONSUL="http://localhost:8500/v1/kv/${NUBIS_STACK}/${NUBIS_ENVIRONMENT}/config"
 
 # Some handy variables here
-EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+INSTANCE_ID=$(curl -s -fq http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE_IP=$(curl -s -fq http://169.254.169.254/latest/meta-data/local-ipv4)
+EC2_AVAIL_ZONE=$(curl -s -fq http://169.254.169.254/latest/meta-data/placement/availability-zone)
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
 # Some bash functions
