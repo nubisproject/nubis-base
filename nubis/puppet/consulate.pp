20b853c0f1def980de99f0671863c008fce1d4bc
limed <limed@mozilla.com>
2015-08-21 17:13:31 -0700

    Removing additional code from consulate.pp since some of the variables are already declared earlier we don't need it. However we should probably clean it up or move it somewhere else

--- nubis/puppet/consulate.pp
+++ nubis/puppet/consulate.pp
@@ -1,23 +1,7 @@
-# Probably not the best idea since we are duplicating code here,
-# perhaps a got spot to put this is in init.pp
-
-$pythondev = $::osfamily ? {
-    'RedHat' => "python27-devel",
-    'Debian' => "python-dev"
-}
-
-$pythonpip = $::osfamily ? {
-    'RedHat' => "python27-pip",
-    'Debian' => "python-pip"
-}
-
-package { $pythondev:
-    ensure => present
-}
-
-package { $pythonpip:
-    ensure => present
-}
+# This all seems very magical but since package {'python': } is already declared in
+# credstash.pp we don't have to worry about all that logic
+#
+# TODO: we should probably cleanup portions of the credstash.pp code and move it to init.pp
 
 python::pip { 'consulate':
     ensure => '0.6.0',
