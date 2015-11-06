a0044ac5e19492cf908f4cfff8e615fcdfbd6c12
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-11-06 11:05:39 -0500

    Ensure lsb_release is installed (dependency for MiG)
    Fixes #231

--- nubis/puppet/mig.pp
+++ nubis/puppet/mig.pp
@@ -41,6 +41,13 @@ class mig (
 
 }
 
+# MiG needs this on RHEL for lsb_release
+if $osfamily == 'redhat' {
+  package {"redhat-lsb-core":
+    ensure => "latest"
+  }
+}
+
 class {'mig':
   version => "20150929",
   build   => "0.9938905"
