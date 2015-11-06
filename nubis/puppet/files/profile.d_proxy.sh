f4a1dbd674a97521843644868a88f6bb95da042a
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-11-06 12:20:43 -0500

    Add EC2 metadata service to no_proxies list
    Fixes #235

--- nubis/puppet/files/profile.d_proxy.sh
+++ nubis/puppet/files/profile.d_proxy.sh
@@ -3,7 +3,7 @@ RV=$?
 if [ ${RV} == 0 ]; then
     export http_proxy="http://proxy.service.consul:3128/"
     export https_proxy="http://proxy.service.consul:3128/"
-    export no_proxy="localhost,127.0.0.1,.localdomain,10.0.0.0/8"
+    export no_proxy="localhost,127.0.0.1,.localdomain,10.0.0.0/8,169.254.169.254"
     export HTTP_PROXY="$http_proxy"
     export HTTPS_PROXY="$https_proxy"
     export NO_PROXY="$no_proxy"
