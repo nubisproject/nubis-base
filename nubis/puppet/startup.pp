34a7095bf010f44a9c3f1ab82a58fab209756624
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-12-18 12:23:34 -0500

    Move Consul startup out of rc.local and into /etc/nubis.d/00-consul
    Fixes #258

--- nubis/puppet/startup.pp
+++ nubis/puppet/startup.pp
@@ -3,6 +3,13 @@ file { '/etc/nubis.d':
   mode    => '0755',
   owner   => 'root',
   group   => 'root',
+}->
+file { '/etc/nubis.d/00-consul':
+  ensure => 'present',
+  mode    => '0755',
+  owner   => 'root',
+  group   => 'root',
+  source  => 'puppet:///nubis/files/consul-bootstrap'
 }
 
 file { '/etc/rc.local':
