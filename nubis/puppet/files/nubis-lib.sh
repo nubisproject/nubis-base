543efe7f7af89af7735a586cadb105969262f0cc
limed <limed@mozilla.com>
2016-05-26 16:54:20 -0700

    output log message to stdout instead of stderr

--- nubis/puppet/files/nubis-lib.sh
+++ nubis/puppet/files/nubis-lib.sh
@@ -111,7 +111,8 @@ function log {
         echo "ERROR: '$BASH_SOURCE' Line: '$LINENO'"
         exit 2
     else
-        $LOGGER_BIN -i --stderr --priority local7.info --tag "$(basename $0)" "${msg}"
+        echo "${msg}"
+        $LOGGER_BIN -i --priority local7.info --tag "$(basename $0)" "${msg}"
     fi
 }
 
