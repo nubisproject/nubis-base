f39122d8a2f928e22086d34f877e55db4b208140
limed <limed@mozilla.com>
2016-09-01 11:10:20 -0700

    Added a way to manage some default users

--- nubis/puppet/nubis_users.pp
+++ nubis/puppet/nubis_users.pp
@@ -13,6 +13,22 @@ file { '/etc/puppet/hiera.yaml':
     source => 'puppet:///nubis/files/hiera.yaml',
 }
 
+file { 'etc/puppet/yaml/nubis_users':
+  ensure => directory,
+  owner  => root,
+  group  => root,
+  mode   => '0744',
+}
+
+file { 'nubis_users_yaml':
+  path    => '/etc/puppet/yaml/nubis_users',
+  owner   => root,
+  group   => root,
+  recurse => 'true',
+  source  => 'puppet:///nubis/files/nubis_users/hiera',
+  require => File['etc/puppet/yaml/nubis_users'],
+}
+
 file { '/etc/confd/conf.d/nubis-users.toml':
     ensure => file,
     owner  => root,
