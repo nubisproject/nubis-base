0562daadec732cb7c4a0f119b367890cee8206ce
limed <limed@mozilla.com>
2016-09-01 16:47:17 -0700

    Added a reload_cmd and also fix various bugs

--- nubis/puppet/nubis_users.pp
+++ nubis/puppet/nubis_users.pp
@@ -10,23 +10,15 @@ file { '/etc/puppet/hiera.yaml':
     owner  => root,
     group  => root,
     mode   => '0644',
-    source => 'puppet:///nubis/files/hiera.yaml',
+    source => 'puppet:///nubis/files/nubis_users/hiera.yaml',
 }
 
 file { '/etc/puppet/yaml/nubis_users':
-  ensure => directory,
-  owner  => root,
-  group  => root,
-  mode   => '0744',
-}
-
-file { 'nubis_users_yaml':
-  path    => '/etc/puppet/yaml/nubis_users',
+  ensure  => directory,
   owner   => root,
   group   => root,
-  recurse => 'true',
+  recurse => true,
   source  => 'puppet:///nubis/files/nubis_users/hiera',
-  require => File['/etc/puppet/yaml/nubis_users'],
 }
 
 file { '/etc/confd/conf.d/nubis-users.toml':
