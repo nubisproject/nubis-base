b4900b91e85beddd52b52655e789c28c44bd2d11
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-10-12 11:41:27 -0400

    When the datadog api key contains the string DISABLED, skip datadog agent startup
    It's noisy and CPU intensive when it's operating with an invalid API key
    
    Fixes #496

--- nubis/puppet/metadata.pp
+++ nubis/puppet/metadata.pp
@@ -11,3 +11,20 @@ file { '/usr/bin/nubis-metadata':
     mode   => '0755',
     source => 'puppet:///nubis/files/nubis-metadata',
 }
+
+file { '/usr/bin/nubis-region':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///nubis/files/nubis-region',
+}
+
+file { '/usr/bin/nubis-availability-zone':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///nubis/files/nubis-availability-zone',
+}
+
