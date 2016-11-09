009e46b2b31bd500bf82788ca0de59e35c94e780
limed <limed@mozilla.com>
2016-11-09 12:06:48 -0800

    Generate puppet
    Will generate a puppet manifest during startup based on the userdata
    that we inject into the node

--- nubis/puppet/nubis_users.pp
+++ nubis/puppet/nubis_users.pp
@@ -1,5 +1,14 @@
-class {'nubis_users::setup':
-  global_admins => ['global-admins'],
-  sudo_users    => ['sudo-users'],
-  nubis_users   => ['users'],
+file { '/usr/local/bin/gen-puppet.sh':
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  source => 'puppet:///nubis/files/gen-puppet.sh',
 }
+
+file { '/etc/nubis.d/99-gen-puppet.sh':
+  ensure  => link,
+  target  => '/usr/local/bin/gen-puppet.sh'
+  require => File['/usr/local/bin/gen-puppet.sh'],
+}
+
