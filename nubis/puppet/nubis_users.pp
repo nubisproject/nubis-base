a42f9a65f29c2a95b134169ff666c3b26a0d09ec
limed <limed@mozilla.com>
2016-11-15 14:45:23 -0800

    Revert "gen-puppet cron"
    This reverts commit 3d3f18fa409203995ebe69c13d437693d587494f.

--- nubis/puppet/nubis_users.pp
+++ nubis/puppet/nubis_users.pp
@@ -5,15 +5,3 @@ file { '/etc/nubis.d/01-gen-puppet.sh':
   mode   => '0755',
   source => 'puppet:///nubis/files/gen-puppet.sh',
 }
-
-cron { 'gen-puppet-watchdog':
-  ensure      => present,
-  command     => '/etc/nubis.d/01-gen-puppet.sh',
-  hour        => '*',
-  minute      => '*/10',
-  user        => 'root',
-  require     => File['/etc/nubis.d/01-gen-puppet.sh'],
-  environment => [
-    'PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/opt/aws/bin'
-  ],
-}
