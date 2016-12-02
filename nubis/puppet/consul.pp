9ed57d5aee3a74ae4720207c95d97ffa90839018
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-12-02 11:33:55 -0500

    puppet lint

--- nubis/puppet/consul.pp
+++ nubis/puppet/consul.pp
@@ -1,10 +1,10 @@
 class { 'consul':
-  version        => '0.7.1',
-  service_enable => false,
-  service_ensure => 'stopped',
-  manage_service => false,
+  version           => '0.7.1',
+  service_enable    => false,
+  service_ensure    => 'stopped',
+  manage_service    => false,
   restart_on_change => false,
-  config_hash    => {
+  config_hash       => {
       'data_dir'             => '/var/lib/consul',
       'log_level'            => 'INFO',
       'ui_dir'               => '/var/lib/consul/ui',
