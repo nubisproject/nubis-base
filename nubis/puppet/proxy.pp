1e364ebc856139a6f3e64bc16740aeedc867f98f
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-10-04 08:58:31 -0400

    cleanup

--- nubis/puppet/proxy.pp
+++ nubis/puppet/proxy.pp
@@ -8,26 +8,3 @@ file {
         group  => 'root',
         source => 'puppet:///nubis/files/profile.d_proxy.sh',
 }
-
-#class { 'tinyproxy':
-#  minspareservers => 0,
-#  maxspareservers => 2,
-#  startservers    => 1,
-#  listen          => '127.0.0.1',
-#  port            => 3128,
-#}
-#
-#tinyproxy::upstream { 'squid':
-#    ensure  => present,
-#    proxy   => 'proxy.service.consul:3128',
-#}
-#
-#tinyproxy::noupstream { 'aws-metadata':
-#    ensure  => present,
-#    match   => '169.254.169.254'
-#}
-#
-#tinyproxy::noupstream { 'consul':
-#    ensure  => present,
-#    match   => '.consul'
-#}
