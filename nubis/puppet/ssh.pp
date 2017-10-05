4c335b4dfdee59d0418e4fba8fe0447eb167a653
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-10-05 10:59:55 -0400

    cleanup ssh

--- nubis/puppet/ssh.pp
+++
@@ -1,6 +0,0 @@
-augeas { 'Disable SSH StrictHostKeyChecking':
-  changes => [
-    'set /files/etc/ssh/ssh_config/Host[*]/StrictHostKeyChecking "no"',
-    'set /files/etc/ssh/ssh_config/Host[*]/UserKnownHostsFile "/dev/null"',
-  ],
-}
