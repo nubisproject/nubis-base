22522151a2f792b32a4c4dab5921ea1e182604d9
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-10-05 10:17:07 -0400

    consul log --

--- nubis/puppet/consul.pp
+++ nubis/puppet/consul.pp
@@ -6,7 +6,7 @@ class { 'consul':
   restart_on_change => false,
   config_hash       => {
       'data_dir'              => '/var/lib/consul',
-      'log_level'             => 'INFO',
+      'log_level'             => 'WARN',
       'ui_dir'                => '/var/lib/consul/ui',
       'client_addr'           => '0.0.0.0',
       'server'                => false,
